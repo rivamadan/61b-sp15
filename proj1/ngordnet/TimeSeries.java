41d6e20aa749de808a47032f56b16afba9a3884a
Riva Madan <riva.madan@berkeley.edu>
2015-03-10 02:59:56 -0700

    finished TimeSeries

--- proj1/ngordnet/TimeSeries.java
+++ proj1/ngordnet/TimeSeries.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.NavigableSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
+
     /** Constructs a new empty TimeSeries. */
     public TimeSeries() {
-        new TreeMap<Integer, T>();
+        super();
     }
 
     /**
@@ -16,9 +18,15 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
      * to be a NavigableSet. This is a private method and you don't have to
      * implement it if you don't want to.
      */
-    private NavigableSet<Integer> validYears(int startYear, int endYear) {
-        return null;
-
+    private NavigableSet<Integer> validYears(TimeSeries<? extends Number> ts) {
+        NavigableSet<Integer> valid = new TreeSet<Integer>();
+        Collection<Integer> thisYear = keySet();
+        for (Integer year : thisYear) {
+            if (ts.containsKey(year)) {
+                valid.add(year);
+            }
+        }
+        return valid;
     }
 
     /**
@@ -26,12 +34,12 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
      * of both end points.
      */
     public TimeSeries(TimeSeries<T> ts, int startYear, int endYear) {
-        ts.subMap(startYear, true, endYear, true);
+        super(ts.subMap(startYear, true, endYear, true));
     }
 
     /** Creates a copy of TS. */
     public TimeSeries(TimeSeries<T> ts) {
-
+        super(ts);
     }
 
     /**
@@ -40,8 +48,28 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
      * IllegalArgumentException.
      */
     public TimeSeries<Double> dividedBy(TimeSeries<? extends Number> ts) {
-        return null;
-
+        TimeSeries<Double> divided = new TimeSeries<Double>();
+        NavigableSet<Integer> sharedYears = validYears(ts);
+        if (ts.size() < this.size() || sharedYears.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        
+        if (ts.size() > this.size()) {
+            Collection<Integer> tsYear = ts.keySet();
+            for (Integer year : tsYear) {
+                sharedYears.add(year);
+            }
+        }
+        
+        for (Integer year : sharedYears) {
+            if (this.containsKey(year)) {
+                double dividedVal = (this.get(year).doubleValue() / ts.get(year).doubleValue());
+                divided.put(year, dividedVal);
+            } else {
+                divided.put(year, 0.0);
+            }
+        }
+        return divided;
     }
 
     /**
@@ -49,15 +77,28 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
      * Double time series (for simplicity).
      */
     public TimeSeries<Double> plus(TimeSeries<? extends Number> ts) {
-        return null;
-
+        TimeSeries<Double> sum = new TimeSeries<Double>();
+        Collection<Integer> yearInt = keySet();
+        Collection<Integer> tsYear = ts.keySet();
+        for (Integer year : tsYear) {
+            sum.put(year, ts.get(year).doubleValue());
+        }
+        for (Integer year : yearInt) {
+            if (ts.containsKey(year)) {
+                double addVal = (this.get(year).doubleValue() + ts.get(year).doubleValue());
+                sum.put(year, addVal);
+            } else {
+                sum.put(year, this.get(year).doubleValue());
+            }
+        }
+        return sum;
     }
 
     /** Returns all years for this time series (in any order). */
     public Collection<Number> years() {
         Collection<Integer> yearInt = keySet();
         Collection<Number> yearNum = new ArrayList<Number>();
-        for(Integer eachYear : yearInt) {
+        for (Integer eachYear : yearInt) {
             yearNum.add(eachYear);
         }
         return yearNum;
@@ -71,7 +112,7 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
     public Collection<Number> data() {
         Collection<T> dataT = values();
         Collection<Number> dataNum = new ArrayList<Number>();
-        for(T eachData : dataT) {
+        for (T eachData : dataT) {
             dataNum.add(eachData);
         }
         return dataNum;
--- proj1/ngordnet/TimeSeriesTest.java
+++ proj1/ngordnet/TimeSeriesTest.java
@@ -41,21 +41,50 @@ public class TimeSeriesTest {
     }
     
     @Test
-    public void testCopyConstructor() {
+    public void testCopyIntervalConstructor() {
         TimeSeries<Double> tsCopy = new TimeSeries<Double>(ts, 1993, 1995);
-        Collection<Number> yearsCopy = tsCopy.years();
-        Collection<Number> dataCopy = tsCopy.data();
-        
-        for (Number yearNumber : yearsCopy) { 
-            int year = yearNumber.intValue();
-            double value = tsCopy.get(yearsCopy);
-            System.out.println("Copy: In the year " + year + " the value was " + value);
-        }
+        TimeSeries<Double> expected = new TimeSeries<Double>();
+        expected.put(1993, 9.2);
+        expected.put(1994, 15.2);
+        expected.put(1995, 16.1);
+        assertEquals(expected, tsCopy);
+        assertNotEquals(tsCopy, ts);
+    }
+    
+    @Test
+    public void testCopyConstructor() {
+        TimeSeries<Double> tsCopy = new TimeSeries<Double>(ts);
+        assertEquals(tsCopy, ts);
+    }
+    
+    @Test
+    public void testDivideBy() {
+        TimeSeries<Integer> ts2 = new TimeSeries<Integer>();
+        ts2.put(1991, 10);
+        ts2.put(1992, 5);   
 
-        for (Number dataNumber : dataCopy) {
-            double datum = dataNumber.doubleValue();
-            System.out.println("Copy: In some year, the value was " + datum);
-        }  
+        TimeSeries<Double> ts3 = new TimeSeries<Double>();
+        ts3.put(1991, 5.0);
+        ts3.put(1992, 2.0);
+        ts3.put(1993, 100.0);
+
+        TimeSeries<Double> tQuotient = ts2.dividedBy(ts3);
+        
+        assertEquals((Double) 2.0, tQuotient.get(1991));
+        assertEquals((Double) 2.5, tQuotient.get(1992));
+        assertEquals((Double) 0.0, tQuotient.get(1993));
+    }
+    
+    @Test
+    public void testPlus() {
+        TimeSeries<Integer> ts2 = new TimeSeries<Integer>();
+        ts2.put(1991, 10);
+        ts2.put(1992, 5);
+        ts2.put(1993, 1);
+
+        TimeSeries<Double> tSum = ts.plus(ts2);
+        assertEquals((Double) 10.0, tSum.get(1991));
+        assertEquals((Double) 8.6, tSum.get(1992));
     }
     
     /* Run the unit tests in this file. */
