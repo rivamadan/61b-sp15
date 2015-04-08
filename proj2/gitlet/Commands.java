38d733738cf756371eb17abea3df019380274e6b
Riva Probe <xreerain@gmail.com>
2015-04-07 21:02:23 -0700

    commit structure

--- proj2/gitlet/Commands.java
+++ proj2/gitlet/Commands.java
@@ -1,11 +1,58 @@
 package gitlet;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /* methods for gitlet's commands */
 
 public class Commands {
-    
+    public static Commit commitList;
+	
     /*initializes gitlet*/
     public static void initialize() {
         FileUtils.createFolder(".gitlet", null);
+        commitList = new Commit();
+    }
+    
+    public static void branch() {
+    	
+    }
+    
+    private static Commit loadCommitList() {
+        Commit allCommits = null;
+        File commitFile = new File("allCommits.ser");
+        if (commitFile.exists()) {
+            try {
+                FileInputStream fileIn = new FileInputStream(commitFile);
+                ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+                allCommits = (Commit) objectIn.readObject();
+            } catch (IOException e) {
+                String msg = "IOException while loading.";
+                System.out.println(msg);
+            } catch (ClassNotFoundException e) {
+                String msg = "ClassNotFoundException while loading.";
+                System.out.println(msg);
+            }
+        }
+        return allCommits;
+    }
+    
+    private static void saveCommitList(Commit allCommits) {
+        if (allCommits == null) {
+            return;
+        }
+        try {
+            File commitFile = new File("allCommits.ser");
+            FileOutputStream fileOut = new FileOutputStream(commitFile);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            objectOut.writeObject(commitFile);
+        } catch (IOException e) {
+            String msg = "IOException while saving.";
+            System.out.println(msg);
+        }
     }
 }
--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -1,8 +1,65 @@
 package gitlet;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+
 /* data structure for holding commits */
 
-public class Commit {
+public class Commit implements Serializable{
+    private class Node {
+    	public Node parent, left, right;
+    	public String branch;
+    	public String message;
+    	public int commitNum;
+    	
+    	public HashSet<String> filesToAdd;
+    	
+    	public Node(Node parent, Node left, Node right, String branch, String message, int commitNum) {
+    		this.parent = parent;
+    		this.left = left;
+    		this.right = right;
+    		this.branch = branch;
+    		this.message = message;
+    		this.commitNum = commitNum;
+    	}
+    }
     
+    public String currHead;
+    public HashMap<String, Node> branches = new HashMap<String, Node>();
+    
+    public Commit() {
+    	Node master = new Node(null, null, null, "master", "initial commit", 0);
+    	updateBranchPointers("master", master);
+    	currHead = "master";
+    }
+
+	private Node getCurrHeadNode() {
+		return branches.get(currHead);
+	}
+	
+	private void updateBranchPointers(String nodeName, Node newHeadNode) {
+		branches.put(nodeName, newHeadNode);
+	}
+    
+    public void addCommit(String msg) {
+    	Node headNode = getCurrHeadNode();
+    	if (headNode.right != null){
+    		headNode.right = new Node(headNode, null, null, currHead, msg, headNode.commitNum+1);
+    		updateBranchPointers(currHead, headNode.right);
+    	} else {
+    		headNode.left = new Node(headNode, null, null, currHead, msg, headNode.commitNum+1);
+    		updateBranchPointers(currHead, headNode.left);
+    	}
+    }
+    
+    public void createBranch(String name) {
+    	updateBranchPointers(name, getCurrHeadNode());
+    	currHead = name;
+    }
+    
+    public void changeBranch(String name) {
+    	currHead = name;
+    }
     
 }
