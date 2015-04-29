996821cbbff25abd5d82a161c6685aa36b023939
Riva Madan <riva.madan@berkeley.edu>
2015-04-29 02:04:47 -0700

    lab12

--- lab12/radix/Sorts.java
+++ lab12/radix/Sorts.java
@@ -37,7 +37,7 @@ public class Sorts {
         for (int i = 0; i < keys.length; i++) {
             int digit = keys[i] & (15 << whichDigit * 4);
             digitSort[counts[digit]] = keys[i];
-            counts[digit]++
+            counts[digit]++;
         }
 
 
