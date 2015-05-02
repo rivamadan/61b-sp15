0d0c2c35605313af65612f4290c76a9893c109ca
Riva Madan <riva.madan@berkeley.edu>
2015-05-02 16:04:34 -0700

    removed unused import

--- proj3/Autocomplete.java
+++ proj3/Autocomplete.java
@@ -1,7 +1,5 @@
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 
 /**
  * Implements autocomplete on prefixes for a given dictionary of terms and
